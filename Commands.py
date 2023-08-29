03c33db9d0f40d467768aec80d1fea0552960df8
Nandani Patel <nandanipatel1406@gmail.com>
2023-08-30 01:21:41 +0530

    final test case with user defined input

--- Commands.py
+++ Commands.py
@@ -2,7 +2,9 @@ from Model import control_commands
 
 initial_position = (0, 0, 0)
 initial_direction = "N"
-commands = ["f", "r", "u","b","l"]
+# commands = ["f", "r", "u","b","l"]
+print("Enter Commands")
+commands=input()
 
 
 final_position, final_direction = control_commands(initial_position, initial_direction, commands)
